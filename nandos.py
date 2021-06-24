0264007a1838d08808c3b5c5effd9ad6e11463bb
Igor Q Moreira <74986053+iqm0@users.noreply.github.com>
2021-06-24 19:02:11 +0100

    Added nandos.py file

--- nandos.py
+++ nandos.py
@@ -26,12 +26,13 @@ time.sleep(3)
 driver.save_screenshot("Screens/" + today.strftime('%Y%m%d') + "_home.png")
 
 # Tap list and locations
+test_xpath = 0
 starters_xpath = '//*[@id="scroll-container"]/div[1]/div/div/div[4]/a[2]'
 halloumi_xpath = '//*[@id="scroll-container"]/div[1]/div/div/div/div[2]/div/div[2]/div[1]/img'
 popup_xpath = '/html/body/div[8]/div/div/footer/div[2]/button'
-cart_xpath = '/html/body/div[8]/div/div/footer/div/button'
-checkout_xpath = '//*[@id="scroll-container"]/div[1]/header/div[1]/div[2]/a[2]/span[2]'
-dismiss_xpath = '/html/body/div[7]/div/div/footer/div/button'
+cart_xpath = '//*[@id="scroll-container"]/div[1]/header/div[1]/div[2]/a[2]'
+checkout_xpath = '//*[@id="scroll-container"]/footer/div[2]/button'
+dismiss_xpath = '/html/body/div[8]/div/div/footer/div/button'
 email_xpath = '//*[@id="capture_signIn_signInEmailAddress"]'
 password_xpath = '//*[@id="capture_signIn_currentPassword"]'
 two_step_xpath = '//*[@id="secondFactorCodeLoginInput"]'
@@ -39,70 +40,76 @@ payment_xpath = '//*[@id="scroll-container"]/footer/div[2]/button'
 second_popup_xpath = '/html/body/div[6]/div/div/footer/div/button'
 pay_button_xpath = '/html/body/div[6]/div/div/footer/div/button'
 
-### Tap 1 - Clicking to see the Starters
+# Defining a method to attempt to wait for a button to be clickable before trying to click it
+def driver_wait(location):
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, location)))
+    return
 
+### Tap 1 - Clicking to see the Starters
+driver_wait(starters_xpath)
 try:
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, starters_xpath)))
-finally:
     starters_button = driver.find_element_by_xpath(starters_xpath)
     starters_button.click()
-
-time.sleep(1)
+except:
+    print('Failed at Tap 1')
 
 ### Tap 2 - Selecting a side (halloumi) from the menu
-
+driver_wait(halloumi_xpath)
 try:
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, halloumi_xpath)))
-finally:
     halloumi_button = driver.find_element_by_xpath(halloumi_xpath)
     halloumi_button.click()
+except:
+    print('Failed at Tap 2')
 
 ### Tap 3 - Add to cart
+driver_wait(popup_xpath)
 try:
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, popup_xpath)))
-finally:
     dismiss_popup = driver.find_element_by_xpath(popup_xpath)
     dismiss_popup.click()
+except:
+    print('Failed at Tap 3')
 
 ### Tap 4 - Go to cart / bag view
+driver_wait(cart_xpath)
 try:
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, cart_xpath)))
-finally:
     view_cart = driver.find_element_by_xpath(cart_xpath)
     view_cart.click()
+except:
+    print('Failed at Tap 4')
 
 ### Tap 5 - Checkout page
+driver_wait(checkout_xpath)
 try:
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, checkout_xpath)))
-finally:
     checkout_page = driver.find_element_by_xpath(checkout_xpath)
     checkout_page.click()
+except:
+    print('Failed at Tap 5')
 
-time.sleep(1)
 
 ### Tap 6 - Dismiss allergy alert pop-up
+driver_wait(dismiss_xpath)
 try:
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, dismiss_xpath)))
-finally:
     dismiss_allergy = driver.find_element_by_xpath(dismiss_xpath)
     dismiss_allergy.click()
+except:
+    print('Failed at Tap 6')
 
 ### Tap 7 - Email entry
+driver_wait(email_xpath)
 try:
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, email_xpath)))
-finally:
     user_email = driver.find_element_by_xpath(email_xpath)
     user_email.send_keys('user email address here')
+except:
+    print('Failed at Tap 7')
 
 ### Tap 8 - Password entry
+driver_wait(password_xpath)
 try:
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, password_xpath)))
-finally:
     user_pass = driver.find_element_by_xpath(password_xpath)
     user_pass.send_keys('user password here')
     user_pass.send_keys(Keys.RETURN)
-
-time.sleep(3)
+except:
+    print('Failed at Tap 8')
 
 ### Fetching the verification code from the email inbox
 entry_code = driver.find_element_by_xpath(two_step_xpath)
@@ -112,29 +119,29 @@ entry_code.send_keys(Keys.RETURN)
 time.sleep(3)
 
 ### Tap 9 - Proceeding to payment
+driver_wait(payment_xpath)
 try:
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, payment_xpath)))
-finally:
     checkout_button = driver.find_element_by_xpath(payment_xpath)
     checkout_button.click()
-    
-time.sleep(3)
+except:
+    print('Failed at Tap 9')
 
 ### Tap 10 - Dismissing another alergy popup, again
+driver_wait(second_popup_xpath)
 try:
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, second_popup_xpath)))
-finally:
     allergy_again = driver.find_element_by_xpath(second_popup_xpath)
     allergy_again.click()
-
-time.sleep(1)
+except:
+    print('Failed at Tap 10')
 
 ### Tap 11 - Click on Apple Pay button if available
+driver_wait(pay_button_xpath)
 try:
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, pay_button_xpath)))
-finally:
     # Further code could be added to throw an error in case the button does not appear
     apple_pay = driver.find_element_by_xpath(pay_button_xpath)
+    apple_pay.click()
+except:
+    print('Apple Pay button not found')
 
 # Save a screenshot of payment method seen
 driver.save_screenshot("Screens/" + today.strftime('%Y%m%d') + "_cart.png")
